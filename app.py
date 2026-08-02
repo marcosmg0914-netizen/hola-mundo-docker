c42f91f53a5b7114fed569b99142d48f4d7ebc37
Marcos <marcosmg0914@gmail.com>
2026-08-02 16:40:57 -0400

    Primer commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def inicio():
+    return "<h1>Hola Mundo desde Docker 🚀</h1>"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
